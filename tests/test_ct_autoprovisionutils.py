272ae0ae005e431983fae9fb4f8fa6b85623c5f2
Naresh Kumar <naresh.kumar8@hpe.com>
2016-06-02 06:23:08 -0700

    Modified the test_ct_autoprovisionutils.py
    Tags: fix, dev
    TG-1273
    
    CR#2695 CIT blocker: test_ct_autoprovisionutils.py is not running, this test is
    being skipped
    
    Resolution: Script has been modified to use lighttpd from ubuntu host
    
    Change-Id: Id28ab2b60bfb223234046cc044638bf07e97dd24
    Signed-off-by: Naresh Kumar <naresh.kumar8@hpe.com>

--- tests/test_ct_autoprovisionutils.py
+++ tests/test_ct_autoprovisionutils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2015 Hewlett Packard Enterprise Development LP
+# Copyright (C) 2015-2016 Hewlett Packard Enterprise Development LP
 # All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -25,61 +25,97 @@ import pytest
 from opsvsi.docker import *
 from opsvsi.opsvsitest import *
 
-HTTP_SERVER = "http://127.0.0.1/autoprovision"
 HTTP_SERVER_ROOT_PATH = "/var/www/servers/www.example.org/pages/"
 SCRIPT_NAME = "autoprovision"
 TEST_ECHO_STRING = "Hello from Autoprovision script"
 LIGHTTPD_CONFIG = "./src/ops-aaa-utils/tests/lighttpd.conf"
 
 
+class myTopo(Topo):
+    """Custom Topology for autoprovision
+    H1[h1-eth0]<--->[1]S1
+    """
+
+    def build(self, hsts=1, sws=1, **_opts):
+        self.hsts = hsts
+        self.sws = sws
+
+        "add list of hosts"
+        for h in irange(1, hsts):
+            host = self.addHost('h%s' % h)
+
+        "add list of switches"
+        for s in irange(1, sws):
+            switch = self.addSwitch('s%s' % s)
+
+        "Add links between nodes based on custom topo"
+        self.addLink('h1', 's1')
+
 class autoprovisionFeatureTest(OpsVsiTest):
     def setupHttpServer(self):
         ''' This function is to setup http server in the ubuntu image
         we are referring to
         '''
-        s1 = self.net.switches[0]
+        h1 = self.net.hosts[0]
 
         #Configure lighttpd conf file and root path of server
-        s1.cmd("mkdir -p " + HTTP_SERVER_ROOT_PATH)
+        h1.cmd("mkdir -p " + HTTP_SERVER_ROOT_PATH)
         with open(LIGHTTPD_CONFIG) as f_config:
             config = f_config.read()
 
-        s1.cmd(config)
+        h1.cmd(config)
 
-        s1.cmd("killall -9 lighttpd")
+        h1.cmd("killall -9 lighttpd")
         sleep(1)
-        out = s1.cmd("lighttpd -t -f ~/lighttpd.conf")
-        out += s1.cmd("echo ")
+        out = h1.cmd("lighttpd -t -f ~/lighttpd.conf")
+        out += h1.cmd("echo ")
 
         if "Syntax OK" in out:
-            out = s1.cmd("lighttpd -f ~/lighttpd.conf")
+            out = h1.cmd("lighttpd -f ~/lighttpd.conf")
         else:
             assert ("Syntax OK" in out), "Failed to setup lighttpd server"
 
         return True
 
+    def getHostIP(self):
+        ''' This function is to get host IP addess
+        '''
+        h1 = self.net.hosts[0]
+        out = h1.cmd("ifconfig eth0")
+        host_IpAddress = out.split("\n")[1].split()[1][5:]
+        return host_IpAddress
+
     def setupAutoprovisionScript(self):
         ''' This function is to setup autoprovision script in http server
         '''
-        s1 = self.net.switches[0]
-        out = s1.cmd("touch " + HTTP_SERVER_ROOT_PATH + SCRIPT_NAME)
+        h1 = self.net.hosts[0]
 
-        s1.cmd("echo \'#!/bin/sh\' >>" + HTTP_SERVER_ROOT_PATH + SCRIPT_NAME)
-        s1.cmd("echo \'#OPS-PROVISIONING\'>>" + HTTP_SERVER_ROOT_PATH +
+        out = h1.cmd("touch " + HTTP_SERVER_ROOT_PATH + SCRIPT_NAME)
+
+        h1.cmd("echo \'#!/bin/sh\' >>" + HTTP_SERVER_ROOT_PATH + SCRIPT_NAME)
+        h1.cmd("echo \'#OPS-PROVISIONING\'>>" + HTTP_SERVER_ROOT_PATH +
                SCRIPT_NAME)
-        s1.cmd("echo echo "+TEST_ECHO_STRING+" >>" + HTTP_SERVER_ROOT_PATH +
+        h1.cmd("echo echo "+TEST_ECHO_STRING+" >>" + HTTP_SERVER_ROOT_PATH +
                SCRIPT_NAME)
 
         return True
 
     def setupNet(self):
         # Create a topology with single openswitch
-        self.net = Mininet(topo=SingleSwitchTopo(k=1, hopts=self.getHostOpts(),
-                           sopts=self.getSwitchOpts()),
-                           switch=VsiOpenSwitch,
-                           host=OpsVsiHost,
-                           link=OpsVsiLink, controller=None,
-                           build=True)
+        # Create a topology with single Openswitch and
+        # a host.
+
+        # Select ops-host image from docker hub, which has lighttpd
+        # installed.
+        self.setHostImageOpts("openswitch/lighttpd-ubuntu")
+
+        topo = myTopo(hsts=1, sws=1, hopts=self.getHostOpts(),
+                      sopts=self.getSwitchOpts(), switch=VsiOpenSwitch,
+                      host=OpsVsiHost, link=OpsVsiLink, controller=None,
+                      build=True)
+
+        self.net = Mininet(topo, switch=VsiOpenSwitch, host=OpsVsiHost,
+                           link=OpsVsiLink, controller=None, build=True)
 
         if self.setupHttpServer():
             info('*** setupHttpServer success\n')
@@ -93,7 +129,11 @@ class autoprovisionFeatureTest(OpsVsiTest):
         '''
         info('\n########## Executing Autoprovision ##########\n')
         s1 = self.net.switches[0]
-        out = s1.cmd("autoprovision "+HTTP_SERVER)
+        h1 = self.net.hosts[0]
+        host_IpAddress = self.getHostIP()
+        HTTP_URL = "http://" + host_IpAddress +"/autoprovision"
+
+        out = s1.cmd("autoprovision "+HTTP_URL)
         out += s1.cmdCLI("end")
 
         if not TEST_ECHO_STRING in out:
@@ -105,8 +145,8 @@ class autoprovisionFeatureTest(OpsVsiTest):
         out = s1.cmdCLI("show autoprovision")
         out += s1.cmdCLI("end")
 
-        if not HTTP_SERVER in out:
-            assert (HTTP_SERVER in out), \
+        if not HTTP_URL in out:
+            assert (HTTP_URL in out), \
                 "Failed in updating autoprovision \
                                         status in DB"
         else:
@@ -114,7 +154,7 @@ class autoprovisionFeatureTest(OpsVsiTest):
 
         info('\n### Executing Autoprovision again, it should not perform '
              '###\n')
-        out = s1.cmd("autoprovision "+HTTP_SERVER)
+        out = s1.cmd("autoprovision "+HTTP_URL)
         out += s1.cmdCLI("end")
 
         if not "Autoprovisioning already completed" in out:
@@ -124,7 +164,6 @@ class autoprovisionFeatureTest(OpsVsiTest):
             info("### Passed:Executing autoprovision script again,"
                  "autoprovision not performed  ###\n")
 
-@pytest.mark.skipif(True, reason="Skipping since this is failing, need to be rewritten")
 class Test_autoprovisionfeature:
     def setup(self):
         pass
